fa83b890521db1f3946844354ba92bc877e99ef8
Bill Seremetis <bill@seremetis.net>
2015-03-02 08:30:51 +0100

    Rename ID to salvattore-grid, because just grid might conflict with peoples code.

--- views-view-salvattore.tpl.php
+++ views-view-salvattore.tpl.php
@@ -9,9 +9,9 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<div id="grid" data-columns>
+<div id="salvattore-grid" data-columns>
 <?php foreach ($rows as $id => $row): ?>
-  <div <?php if ($classes_array[$id]) print 'class="' . $classes_array[$id] . '"'; ?>">
+  <div <?php if ($classes_array[$id]) print 'class="' . $classes_array[$id] . '"'; ?>>
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
